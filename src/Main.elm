238abeb165d3ae504ee87b5e38d48c52ce245ceb
Goryudyuma <umaretekyoumade@gmail.com>
2018-10-08 06:05:35 +0900

    add firebase

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (Model, Msg(..), init, main, update, view)
+port module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Html exposing (Html, div, h1, img, text, ul, li, input, a)
@@ -7,7 +7,8 @@ import List exposing (filter, map, length, indexedMap,maximum, sortBy, head)
 import Tuple exposing(first,second )
 import Maybe exposing (withDefault)
 import Html.Events exposing (onInput, onClick)
-
+import Json.Encode exposing (encode)
+import Json.Decode
 
 
 ---- MODEL ----
@@ -20,6 +21,20 @@ type alias Model =
         todoAddTextBox:String
     }
 
+toJsonModel : Model -> Json.Encode.Value
+toJsonModel model =
+    Json.Encode.object
+    [
+        ("todoList", Json.Encode.list toJsonTodo model.todoList),
+        ("lavelList", Json.Encode.list toJsonLavel model.lavelList)
+    ]
+fromJsonModel : Json.Decode.Decoder Model
+fromJsonModel =
+    Json.Decode.map3 Model
+        (Json.Decode.field "todoList" (Json.Decode.list fromJsonTodo))
+        (Json.Decode.field "lavelList" (Json.Decode.list fromJsonLavel))
+        (Json.Decode.succeed "")
+
 type alias Todo =
     {
         id:Int,
@@ -28,22 +43,54 @@ type alias Todo =
         priority:Int,
         lavelId:Int
     }
+toJsonTodo : Todo -> Json.Encode.Value
+toJsonTodo todo =
+    Json.Encode.object
+    [
+        ("id", Json.Encode.int todo.id),
+        ("title", Json.Encode.string todo.title),
+        ("content", Json.Encode.string todo.content),
+        ("priority", Json.Encode.int todo.priority),
+        ("lavelId", Json.Encode.int todo.lavelId)
+    ]
+fromJsonTodo : Json.Decode.Decoder Todo
+fromJsonTodo =
+    Json.Decode.map5 Todo
+        (Json.Decode.field "id" Json.Decode.int)
+        (Json.Decode.field "title" Json.Decode.string)
+        (Json.Decode.field "content" Json.Decode.string)
+        (Json.Decode.field "priority" Json.Decode.int)
+        (Json.Decode.field "lavelId" Json.Decode.int)
 
 type alias Lavel = {
         id:Int,
         lavel:String
     }
+toJsonLavel : Lavel -> Json.Encode.Value
+toJsonLavel lavel =
+    Json.Encode.object
+    [
+        ("id", Json.Encode.int lavel.id),
+        ("lavel", Json.Encode.string lavel.lavel)
+    ]
+fromJsonLavel : Json.Decode.Decoder Lavel
+fromJsonLavel =
+    Json.Decode.map2 Lavel
+        (Json.Decode.field "id" Json.Decode.int)
+        (Json.Decode.field "lavel" Json.Decode.string)
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( {
+initModel = {
         todoList=[],
         lavelList=[
-            {id=0,lavel="Todo"}, 
+            {id=0,lavel="Todo"},
             {id=1,lavel="Doing"},
             {id=2,lavel="Done"}],
         todoAddTextBox=""
-    }, Cmd.none )
+    }
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    (initModel , Cmd.none )
 
 
 
@@ -59,41 +106,127 @@ type Msg
     | TodoAddTextBoxChange String
     | TodoLevelUp Int
     | TodoLevelDown Int
+    | GetUpdate String
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        LavelListLavelChange n s -> ({model | lavelList = 
-                map (\m -> if m.id == n then {m | lavel = s} else m) model.lavelList
-            }, Cmd.none)
-        TodoPriorityChange n m-> ({model| todoList =
-                map (\t -> if t.id == n then {t | id = m} else t) model.todoList
-            }, Cmd.none)
-        TodoTitleChange n s-> ({model| todoList =
-                map (\t -> if t.id == n then {t | title = s} else t) model.todoList
-            }, Cmd.none)
-        TodoContentChange n s-> ({model| todoList =
-                map (\t -> if t.id == n then {t | content = s} else t) model.todoList
-            }, Cmd.none)
-        TodoAdd -> ({model | todoList =
-                {
-                    id = 1 + withDefault 0 (maximum (map (\t -> t.id) model.todoList)),
-                    title = model.todoAddTextBox,
-                    content="",
-                    priority=0,
-                    lavelId=0
-                } :: model.todoList,
-                todoAddTextBox = ""
-            }, Cmd.none)
-        TodoDel n -> ({model | todoList = filter (\t -> t.id /= n)model.todoList}, Cmd.none)
-        TodoAddTextBoxChange s -> ({model | todoAddTextBox = s}, Cmd.none)
-        TodoLevelUp n -> ({model| todoList =
-                map (\t -> if t.id == n then {t | lavelId = min 2 (t.lavelId + 1)} else t) model.todoList
-            }, Cmd.none)
-        TodoLevelDown n -> ({model| todoList =
-                map (\t -> if t.id == n then {t | lavelId = max 0 (t.lavelId - 1)} else t) model.todoList
-            }, Cmd.none)
+    let
 
+        updateJsonCmd : Model -> Cmd Msg
+        updateJsonCmd sendModel = sendUpdate (encode 0 (toJsonModel sendModel))
+
+        send : Model -> ( Model, Cmd Msg )
+        send newModel = (newModel, updateJsonCmd newModel)
+
+        doNotSend : Model -> ( Model, Cmd Msg )
+        doNotSend newModel = (newModel, Cmd.none)
+    in
+
+    case msg of
+        LavelListLavelChange n s ->
+            let
+                newModel =
+                    {
+                        model | lavelList =
+                        map (\m -> if m.id == n then {m | lavel = s} else m) model.lavelList
+                    }
+            in
+                send newModel
+
+        TodoPriorityChange n m ->
+            let
+                newModel =
+                    {
+                        model| todoList =
+                        map (\t -> if t.id == n then {t | id = m} else t) model.todoList
+                    }
+            in
+                send newModel
+
+        TodoTitleChange n s ->
+            let
+                newModel =
+                    {
+                        model| todoList =
+                        map (\t -> if t.id == n then {t | title = s} else t) model.todoList
+                    }
+            in
+                send newModel
+
+        TodoContentChange n s ->
+            let
+                newModel =
+                    {
+                        model| todoList =
+                        map (\t -> if t.id == n then {t | content = s} else t) model.todoList
+                    }
+            in
+                send newModel
+
+        TodoAdd ->
+            let
+                newModel =
+                    {
+                        model |
+                            todoList =
+                                {
+                                    id = 1 + withDefault 0 (maximum (map (\t -> t.id) model.todoList)),
+                                    title = model.todoAddTextBox,
+                                    content="",
+                                    priority=0,
+                                    lavelId=0
+                                } :: model.todoList,
+                        todoAddTextBox = ""
+                    }
+            in
+                send newModel
+
+        TodoDel n ->
+            let
+                newModel =
+                    {
+                        model | todoList = filter (\t -> t.id /= n)model.todoList
+                    }
+            in
+                send newModel
+
+        TodoAddTextBoxChange s ->
+            let
+                newModel =
+                    {model | todoAddTextBox = s}
+            in
+                doNotSend newModel
+
+        TodoLevelUp n ->
+            let
+                newModel =
+                    {
+                        model| todoList =
+                        map (\t -> if t.id == n then {t | lavelId = min 2 (t.lavelId + 1)} else t) model.todoList
+                    }
+            in
+                send newModel
+
+        TodoLevelDown n ->
+            let
+                newModel =
+                    {
+                        model| todoList =
+                        map (\t -> if t.id == n then {t | lavelId = max 0 (t.lavelId - 1)} else t) model.todoList
+                    }
+            in
+                send newModel
+
+        GetUpdate s ->
+            (
+                case (Json.Decode.decodeString fromJsonModel s) of
+                    Err e ->
+                        model
+                    Ok newModel ->
+                        {
+                            newModel | todoAddTextBox = model.todoAddTextBox
+                        }
+            , Cmd.none)
 
 
 
@@ -111,42 +244,44 @@ viewTodo todo lavelList=
             li[][text (String.fromInt t.priority)],-}
             if todo.lavelId /= 0 then
                 li[style "display" "table-cell"][
-                    input[type_ "button", name "Prev", 
-                    value (withDefault "" (head (map (\l -> l.lavel) (filter (\l -> l.id == todo.lavelId - 1) lavelList)))), 
+                    input[type_ "button", name "Prev",
+                    value (withDefault "" (head (map (\l -> l.lavel) (filter (\l -> l.id == todo.lavelId - 1) lavelList)))),
                     onClick (TodoLevelDown todo.id)][]
                 ]
             else
                 text ""
             ,
-            li[style "display" "table-cell"][
-                input[type_ "button", name "Delete", 
-                value "Delete", 
-                onClick (TodoDel todo.id)][]
-            ],
-            if todo.lavelId /= (length lavelList) - 1 then 
+            if todo.lavelId /= (length lavelList) - 1 then
                 li[style "display" "table-cell"][
-                    input[type_ "button" , name "Next", 
-                    value (withDefault "" (head (map (\l -> l.lavel) (filter (\l -> l.id == todo.lavelId + 1) lavelList)))),  
+                    input[type_ "button" , name "Next",
+                    value (withDefault "" (head (map (\l -> l.lavel) (filter (\l -> l.id == todo.lavelId + 1) lavelList)))),
                     onClick (TodoLevelUp todo.id)][]
                 ]
             else
                 text ""
+        ],
+        ul[style "list-style-type" "none"][
+            li[style "display" "table-cell"][
+                input[type_ "button", name "Delete",
+                value "Delete",
+                onClick (TodoDel todo.id)][]
+            ]
         ]
     ]
-                
+
 
 
 view : Model -> Html Msg
 view model =
     div []
         [
-        input [ placeholder "New Task" , value model.todoAddTextBox, onInput TodoAddTextBoxChange] [], 
+        input [ placeholder "New Task" , value model.todoAddTextBox, onInput TodoAddTextBoxChange] [],
         input[type_ "button", name "AddTask", value "Add",  onClick TodoAdd][],
         div[][ul [style "list-style-type" "none"]
         ( map (\m -> li[style "display" "table-cell"][
             text m.lavel,
             ul [style "list-style-type" "none"](
-                map (\t -> 
+                map (\t ->
                     viewTodo t model.lavelList
                     )
                 (sortBy (\t -> t.priority) (filter (\t -> t.lavelId == m.id)model.todoList))
@@ -155,9 +290,17 @@ view model =
         ]
 
 
+
+---- PORT ----
+
+port sendUpdate : String -> Cmd msg
+port getUpdate : (String -> msg) -> Sub msg
+
+
+
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    getUpdate GetUpdate
 
 
 main =
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/database");
+require("firebase/firestore");
+require("firebase/messaging");
+require("firebase/functions");
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD9Yd-QTn1zTuF8Sx_xTR145yVCIGXCUEQ",
+  authDomain: "elm-todo-063-jp.firebaseapp.com",
+  databaseURL: "https://elm-todo-063-jp.firebaseio.com",
+  projectId: "elm-todo-063-jp",
+  storageBucket: "elm-todo-063-jp.appspot.com",
+  messagingSenderId: "946848464130"
+};
+firebase.initializeApp(config);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+var provider = new firebase.auth.GithubAuthProvider();
+
+var user
+
 require('./main.scss');
 
 const Elm = require('./Main.elm');
@@ -5,4 +30,40 @@ const mountNode = document.getElementById('main');
 
 const app = Elm.Elm.Main.init({
   node: document.getElementById('main')
+});
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  user = result.user;
+  // ...
+  var datastore = firebase.database().ref('data/' + user.uid);
+  datastore.on('value', function(snapshot) {
+    console.log("snapshot: "+snapshot.val())
+    app.ports.getUpdate.send(snapshot.val())
+  });
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+// Elm -> JS
+function writeData(data) {
+  if(user){
+    firebase.database().ref('data/' + user.uid).set(data);
+  }else{
+    console.log("Please Login")
+  }
+}
+
+app.ports.sendUpdate.subscribe(function(str) {
+  console.log("json: "+str);
+  writeData(str)
 });
